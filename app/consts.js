613d7cd17f6247800cac5aae9276684f7d4fdfe0
rom5995 <30599074+rom5995@users.noreply.github.com>
2018-04-27 11:57:16 +0300

    no message

--- app/consts.js
+++ app/consts.js
@@ -2,5 +2,8 @@ const serverUrl = 'http://localhost/PersonalTrainerServer';
 
 angular.module('personalTrainer').
 constant('consts', {
+    userApi: `${serverUrl}/user`,
+    registerApi: `${serverUrl}/user/Register`,
+    loginApi: `${serverUrl}/user/Login`,
     userApi: `${serverUrl}/user`
 });
--- app/loggedInMain/loggedInMainController.js
+++ app/loggedInMain/loggedInMainController.js
@@ -2,9 +2,5 @@ angular.module('personalTrainer').
 controller('loggedInMainController', function($scope, $location, $http, consts, userService) {
     const ctrl = this;
 
-    userService.getUser({ id: 4 } ,function(usr) {
-        console.log(usr);
-    });
-
     return ctrl;
 });

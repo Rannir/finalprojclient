d7788d4cf51007fef015d9915adb355d46c749fd
Ran <ranraphaelnir@gmail.com>
2018-04-27 12:28:38 +0300

    do it with user service

--- app/loggedInMain/loggedInMainController.js
+++ app/loggedInMain/loggedInMainController.js
@@ -1,10 +1,9 @@
 angular.module('personalTrainer').
 controller('loggedInMainController', function($scope, $location, $http, consts, userService) {
     const ctrl = this;
-    
+
     ctrl.labels = ["Starting weight", "Current weight", "Goal weight"];
-    ctrl.data = [80, 83, 85];
-    //$scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    ctrl.data = [userService.getUser().Goal.StartingWeight, userService.getUser().Measurement.Weight, userService.getUser().Goal.GoalWeight];
     ctrl.series = ['Goals'];
 
     ctrl.progress = function () {

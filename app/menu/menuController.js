af891727a37bfa0b433aa412684f09399dc7fc1f
Ran <ranraphaelnir@gmail.com>
2018-04-27 14:08:43 +0300

    fix conflicts

--- app/menu/menuController.js
+++ app/menu/menuController.js
@@ -1,19 +1,10 @@
-angular.module('personalTrainer').
-<<<<<<< HEAD
-controller('menuController', function($scope, $http, userService) {
-=======
-controller('menuController', function($scope, $http, consts, userService) {
->>>>>>> 2b62294fb3cfcad7ea1b23528e2c3f7bfebe9713
+angular.module('personalTrainer').controller('menuController', function($scope, $http, consts, userService) {
     const ctrl = this;
     ctrl.selectedMenu = null;
     ctrl.menuIndex = 0;
 
     ctrl.load = function() {
-<<<<<<< HEAD
-        $http.get('http://localhost/PersonalTrainerServer/Menu')
-=======
         $http.get(`${consts.algApi}/${userService.getUser().UserID}`)
->>>>>>> 2b62294fb3cfcad7ea1b23528e2c3f7bfebe9713
             .then(function(response){
                 ctrl.menus = response.data;
                 ctrl.selectedMenu = ctrl.menus[ctrl.menuIndex];

0fa1de846e5181c83094dedae7fa28f0cda7b9c3
BARPC\BAR <bargitchen@gmail.com>
2018-06-02 18:14:40 +0300

    תיקון פרטי משתמשת ושמירה של עדכון מטרה ומשקל בקאש

--- app/loggedInMain/loggedInMainController.js
+++ app/loggedInMain/loggedInMainController.js
@@ -2,16 +2,9 @@ angular.module('personalTrainer').
 controller('loggedInMainController', function($scope, $location, $http, consts, userService) {
     const ctrl = this;
     
-    ctrl.editMenu = function() {
-        userService.getUser(null, function(usr){
-
-            $http.get(`${consts.menuApi}/` + usr.Goal.MenuID)
-                .then(function(response){
-                    userService.setMenu(response.data);
-                    $location.path("/editMenu");
-            });
-        });
-    }
+    userService.getUser(null, function(usr){
+        ctrl.user = usr;
+    });
     
     return ctrl;
 });
